a1bc13e5ad8234f3da4c672e1b777e16c9664eee
Mohamed Ubeid <mubaid@vast.ps>
2024-08-01 16:37:26 +0300

    event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,17 +7,33 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
-  name = "john doe";
+  // name = "john doe";
+  addToCart: number = 0;
   product = {
     name: 'iPhone',
     price: 789,
     color: 'black',
     discount: 8.5,
-    inStock: 10,
+    inStock: 5,
     pImage: '/assets/images/iphone.png', 
   }
 
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount / 100);
   }
+
+  // onNameChnage(event: any) {
+  //   // console.log(event.target.value)
+  //   // this.name = event.target.value;
+  // }
+
+  decrementCartValue() {
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+  incrementCartValue() {
+    if(this.addToCart < this.product.inStock)
+    this.addToCart++;
+    }
 }

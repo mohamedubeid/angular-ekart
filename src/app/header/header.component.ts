8deccfb5f976428c78fceb3873470a854ec527f2
MohamedUbeid9 <mubaid@vast.ps>
2024-08-01 09:36:22 +0300

    styling template using styles array

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styles: ['a{text-decoration: none; margin: 0 10px}','button{padding: 10px 20px}', '.ekart--header{width:100%; height: 70px}', '#id{}']
 })
 export class HeaderComponent{
 
